f2fefac5b2732fb57564db746ae4c545c2e75131
lucasAMBR <lucas.silvaambrosio_9119@outlook.com>
2025-07-28 12:06:04 -0300

    Base Project

---
+++ src/main/java/com/LHDev/PulseChating/PulseChatingApplication.java
@@ -0,0 +1,13 @@
+package com.LHDev.PulseChating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseChatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulseChatingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LHDev/PulseChating/PulseChatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LHDev.PulseChating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseChatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
